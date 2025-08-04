dd177c0a9a581f3444a9b66ac566a14c11a7d1e4
Harshabellala26 <harshabellala23@gmail.com>
2025-08-05 03:13:02 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Get the root element
 const container = document.getElementById('root');
-
-// Create a root only if the container exists
 if (container) {
   const root = createRoot(container);
   root.render(
@@ -17,7 +14,4 @@ if (container) {
   );
 }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
